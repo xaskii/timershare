ac4e9f4cce5bbce4b35ab9e6600379170ae86466
Xaskii <21989858+Xaskii@users.noreply.github.com>
2023-02-15 13:58:43 -0500

    added environment variables (dotenv), and types

--- websocket-backend/src/index.ts
+++ websocket-backend/src/index.ts
@@ -1,6 +1,9 @@
 import { Server, Socket } from 'socket.io'
 import { DefaultEventsMap } from 'socket.io/dist/typed-events'
+import { CLIENT_RENEG_WINDOW } from 'tls'
 import { v4 as uuidv4 } from 'uuid'
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 interface SocketData {
   roomId: string
@@ -13,13 +16,16 @@ const io = new Server<
   SocketData
 >({
   cors: {
-    origin: 'http://localhost:3000',
+    origin: process.env.CLIENT_URL,
   },
 })
 const db = new Set<string>()
 const randomId = () => uuidv4()
 
 io.use((socket, next) => {
+  // TODO: prob switching to actual websockets later so I'll rewrite this
+  // leaving this here so there's a socketid for the application,
+  // not sure how i'm going to implement rooms
   const roomId = socket.handshake.auth.sessionID
   if (roomId) {
     // find existing session
@@ -32,9 +38,10 @@ io.use((socket, next) => {
 })
 
 io.on('connection', (socket) => {
-  console.log(`${socket.id} connected`)
+  console.log(socket.handshake.headers)
 
-  socket.on('ready', () => {
+  socket.on('extend hand', () => {
+    socket.emit('shake-hand')
     socket.emit('info', {
       connected: true,
       db: db,
@@ -43,16 +50,24 @@ io.on('connection', (socket) => {
   })
 
   socket.on('ping', () => {
-    socket.emit('pong')
+    socket.emit('pong', {
+      date: new Date(),
+    })
+    socket.emit(`ping response: ${new Date().toISOString()}`)
   })
-  socket.on('create-room', () => {
+  socket.on('join-room', () => {
     const id = String(uuidv4())
-    db.add(id)
+    // if (db.has(id)) {
+    //   return
+    // }
+
+    // db.add(id)
     socket.join(id)
-    socket.emit('connect')
+    socket.emit(`room(${id}) joined`)
   })
-  socket.on('disconnect', () => {
+
+  socket.on('disconnect', ({}) => {
     console.log(`${socket.id} disconnected`)
   })
 })
-io.listen(3001)
+io.listen(Number(process.env.PORT))
